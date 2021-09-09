d84adc2449fee2c7a1717f7dcdd3d0743c231b03
Theseregrets <rishitashaw49@gmail.com>
2021-09-09 17:00:06 +0530

    templates cached

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -8,12 +8,12 @@ import (
 
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplates(w, "about.page.html")
+	render.RenderTemplates(w, "about.page.tmpl")
 	
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplates(w, "about.page.html")
+	render.RenderTemplates(w, "about.page.tmpl")
 
 }
 
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -4,14 +4,60 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"path/filepath"
 )
 
+var functions = template.FuncMap{
+
+}
 
 func RenderTemplates(w http.ResponseWriter, tmplName string){
+
+	_, err := RenderTemplatesTest(w)
+	if err != nil {
+		fmt.Println("error in getting template cache", err)
+	}
+
 	parsedTemplate, _ := template.ParseFiles("./templates/"+tmplName)
-	err := parsedTemplate.Execute(w, nil)
+	err = parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template", err)
 		return
 	}
+}
+
+func RenderTemplatesTest(w http.ResponseWriter) (map[string]*template.Template, error){
+	
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		fmt.Println("page is currently on", page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches)>0{
+			ts, err = ts.ParseGlob("./templates/*layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+	return myCache, nil
 }
